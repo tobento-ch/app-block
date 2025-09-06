e322c1c05fb5c933446794a171e460c701bf030a
TOBENTOch <info@tobento.ch>
2025-09-06 16:00:09 +0200

    fixed for mail css

--- resources/views/block/mail/hero-editable.php
+++ resources/views/block/mail/hero-editable.php
@@ -1,8 +1,8 @@
 <?php
 $attributes = $block->options()->toTagAttributes();
-$attributes->add('class', ['block', 'block-hero', 'cols']);
+$attributes->add('class', ['block', 'block-hero-mail']);
 ?>
 <div<?= $attributes ?>>
-    <div class="col-6 content" data-editor><?= $view->sanitizeHtml($block->html()) ?></div>
-    <div class="col-6"><?= $pictureTag ?></div>
+    <div class="content" data-editor><?= $view->sanitizeHtml($block->html()) ?></div>
+    <div class="mt-s"><?= $pictureTag ?></div>
 </div>
