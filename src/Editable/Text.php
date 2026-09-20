7fac1ca90bdd60e5f5a9bf8a99e954f1a94072ae
TOBENTOch <info@tobento.ch>
2026-09-20 13:20:10 +0200

    using AbstractFields

--- src/Editable/Text.php
+++ src/Editable/Text.php
@@ -10,11 +10,9 @@
  */
 
 declare(strict_types=1);
- 
+
 namespace Tobento\App\Block\Editable;
 
-use Tobento\App\Block\Editable\Option\OptionsInterface;
-use Tobento\App\Block\EditableBlockInterface;
 use Tobento\App\Crud\Action\ActionInterface;
 use Tobento\App\Crud\Field;
 use Tobento\App\Crud\Field\FieldInterface;
@@ -22,21 +20,36 @@ use Tobento\App\Crud\Field\FieldsInterface;
 use function Tobento\App\Translation\trans;
 
 /**
- * Text
+ * Text Editable Block
  */
-final class Text implements EditableBlockInterface
-{
+class Text extends AbstractFields
+{    
     /**
-     * Create a new Text instance.
+     * Returns the block type used for registration.
      *
-     * @param OptionsInterface $options
+     * @return string
      */
-    public function __construct(
-        private OptionsInterface $options,
-    ) {}
+    public function type(): string
+    {
+        return 'text';
+    }
     
     /**
-     * Returns the title.
+     * Returns the configured block fields.
+     *
+     * @param ActionInterface $action
+     * @return iterable<FieldInterface>|FieldsInterface
+     */
+    protected function configureBlockFields(ActionInterface $action): iterable|FieldsInterface
+    {
+        yield new Field\Textarea(name: 'translation', label: trans('Text'))
+            ->group(trans('Text'))
+            ->validate('string')
+            ->translatable();
+    }
+
+    /**
+     * Returns the block title.
      *
      * @return string
      */
@@ -44,9 +57,9 @@ final class Text implements EditableBlockInterface
     {
         return trans('Text');
     }
-    
+
     /**
-     * Returns the description.
+     * Returns the block description.
      *
      * @return string
      */
@@ -54,7 +67,7 @@ final class Text implements EditableBlockInterface
     {
         return trans('Add a text section.');
     }
-    
+
     /**
      * Returns the icon. Any data from unsecure source must be HTML escaped.
      *
@@ -63,44 +76,5 @@ final class Text implements EditableBlockInterface
     public function icon(): string
     {
         return '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19 10h-14" /><path d="M5 6h14" /><path d="M14 14h-9" /><path d="M5 18h6" /><path d="M18 15v6" /><path d="M15 18h6" /></svg>';
-    }
-    
-    /**
-     * Returns the default block.
-     *
-     * @return array<string, mixed>
-     */
-    public function defaultBlock(): array
-    {
-        return ['type' => 'text', 'translation' => ['en' => '']];
-    }
-    
-    /**
-     * Returns the configured fields.
-     *
-     * @param ActionInterface $action
-     * @return iterable<FieldInterface>|FieldsInterface
-     */
-    public function configureFields(ActionInterface $action): iterable|FieldsInterface
-    {
-        return [
-            new Field\Textarea(name: 'translation', label: trans('Text'))
-                ->group(trans('Text'))
-                ->validate('string')
-                ->translatable(),
-            ...$this->options->configureFields($action, $this),
-        ];
-    }
-    
-    /**
-     * Map the block to the fields.
-     *
-     * @param array<string, mixed> $block
-     * @param ActionInterface $action
-     * @return array<string, mixed>
-     */
-    public function toFields(array $block, ActionInterface $action): array
-    {
-        return $block;
     }
 }
